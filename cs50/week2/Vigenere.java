36f530bd40d5f9eed25dbf0a543ca0334332126f
adtm <tomas.eglinskas@gmail.com>
2017-02-22 00:34:20 +0200

    Refactored vigenere week2

--- cs50/week2/Vigenere.java
+++ cs50/week2/Vigenere.java
@@ -1,7 +1,5 @@
 package week2;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Vigenere {
@@ -11,7 +9,7 @@ public class Vigenere {
     Scanner input = new Scanner(System.in);
     String keyword = inputKeyword(input);
 
-    if(!checkIfLetterInput(keyword)) return;
+    if (!checkIfLetterInput(keyword)) return;
 
     String word = inputWord(input);
 
@@ -35,49 +33,55 @@ public class Vigenere {
     return true;
   }
 
-  private static String cypherWord(String word, String keyword){
+  private static String cypherWord(String word, String keyword) {
 
-    int valueChecker;
-    List<Integer> keywordIndexValues = new ArrayList<>();
-    for (int j = 0; j < keyword.length(); j++) {
-      valueChecker = 0;
-      for(char i = 'a'; i <= 'z'; i++) {
-        valueChecker++;
-        if (keyword.toLowerCase().charAt(j) == i) {
-          keywordIndexValues.add(valueChecker - 1);
-        }
-      }
-    }
+    int keywordValueArray[] = putKeywordValues(keyword);
 
-    int counter = 0;
-    int valueInPoints = 0;
+    int index = 0;
     String encryptedAnswer = "";
     for (int i = 0; i < word.length(); i++) {
-
-      if(counter == keyword.length()) counter = 0;
-
       char digit;
-      if (Character.isLetter(word.charAt(i))){
-        digit = (char) (word.charAt(i) + keywordIndexValues.get(counter) % 26);
-        if(!Character.isLetter(digit)){
+      char currentCharacter = word.charAt(i);
 
+      if (checkIfIsLetterOneCharacter(currentCharacter)) {
+        digit = (char) (currentCharacter + keywordValueArray[index] % 26);
 
-          valueChecker = 0;
-          for(char z = 'a'; z <= 'z'; z++) {
-            valueChecker++;
-            if (word.charAt(i) == z) valueInPoints = valueChecker;
-          }
-
-          digit = (char) ( 'a' + (keywordIndexValues.get(counter) - (27-valueInPoints)) % 26);
-          System.out.println(keywordIndexValues.get(counter) + " " + (26-valueInPoints));
+        if (!Character.isLetter(digit)) {
+          digit = (char) ('a' + (keywordValueArray[index] -
+            (26 - getCharValueInAlphabet(currentCharacter))) % 26);
         }
-        counter++;
+        index++;
       } else {
         digit = (word.charAt(i));
       }
+
       encryptedAnswer += digit;
+      if (index == keyword.length()) index = 0;
     }
+
     return encryptedAnswer;
   }
 
+  private static int[] putKeywordValues(String keyword) {
+    int keywordValueArray[] = new int[keyword.length()];
+    for (int j = 0; j < keyword.length(); j++) {
+      keywordValueArray[j] = getCharValueInAlphabet(keyword.charAt(j));
+    }
+    return keywordValueArray;
+  }
+
+  private static boolean checkIfIsLetterOneCharacter(char letter) {
+    return Character.isLetter(letter);
+  }
+
+  private static int getCharValueInAlphabet(char checkingWord) {
+    int valueChecker = 0;
+    for (char i = 'a'; i <= 'z'; i++) {
+      valueChecker++;
+      if (checkingWord == i) {
+        return valueChecker - 1;
+      }
+    }
+    return 0;
+  }
 }
