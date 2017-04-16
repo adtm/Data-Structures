10dbfe2cfef4c7fee9063419dcb096e14399e4bf
adtm <tomas.eglinskas@gmail.com>
2017-04-16 15:39:25 +0300

    0 1 segreregation - basic

--- data_structures_learning/array/Main.java
+++ data_structures_learning/array/Main.java
@@ -2,9 +2,25 @@ package array;
 
 public class Main {
   public static void main(String[] args) {
-    int[] arr = {1,1,1,2,2};
+    int[] arr = {0, 1, 0, 1, 0, 0, 1, 1, 1, 0};
 
-    System.out.println(majority(arr, 1));
+    segregate(arr);
+  }
+
+  private static void segregate(int[] arr) {
+    int left = 0;
+    int right = arr.length - 1;
+
+    while(right > left) {
+      arr[left] = 0;
+      arr[right] = 1;
+      left++;
+      right--;
+    }
+
+    for(int i = 0; i < arr.length; i++)
+      System.out.println(arr[i]);
+
   }
 
   private static int deleteElement(int key, int[] arr, int n) {
