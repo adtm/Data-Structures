def93e78ab52d9e0d1629d89741c0aa2264c1b10
adtm <tomas.eglinskas@gmail.com>
2017-04-28 22:47:42 +0300

    Stock Problem Stack

--- data_structures_learning/Stack/Exercise/Main.java
+++ data_structures_learning/Stack/Exercise/Main.java
@@ -3,12 +3,23 @@ package g_stack;
 class Main {
 
   public static void main(String[] args) {
-    StackByQueue stackByQueue = new StackByQueue();
-    stackByQueue.push(1);
-    stackByQueue.push(2);
-    stackByQueue.push(3);
-    stackByQueue.push(4);
-    stackByQueue.traverse();
+    int[] prices = {10, 4, 5, 90, 120, 80};
+    stockProblemStack(prices);
+  }
+
+  private static void stockProblemStack(int[] prices) {
+    Stack stack = new Stack();
+    stack.push(0);
+    int[] S = new int[prices.length];
+    S[0] = 1;
+    for (int i = 1; i < prices.length; i++) {
+      while (!stack.isEmpty() && prices[stack.peek()] <= prices[i])
+        stack.pop();
+      S[i] = stack.isEmpty() ? (i+1) : (i - stack.peek());
+      stack.push(i);
+    } 
+    for (int i: S)
+      System.out.println(i);
   }
 
   private static boolean pairWise(String s) {
